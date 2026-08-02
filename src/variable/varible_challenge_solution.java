94912c25845c30c163790131e3b3e56013d3a654
Shiva Sharma <ss12@umbc.edu>
2026-08-02 09:57:29 -0400

    variable challenge solution

--- src/variable/varible_challenge_solution.java
+++ src/variable/varible_challenge_solution.java
@@ -7,15 +7,17 @@ Student GPA
 Store values inside them
 Print the values in a complete sentence
 
-expected output Student John is 20 years old and has a GPA of 3.5
+expected output: Student John is 20 years old and has a GPA of 3.5
  */
 public  class varible_challenge_solution {
+
 
     void main() {
 
         String name = "John";
         int age = 20;
         double gpa = 3.5;
+
         System.out.println("Student " + name + " is " + age +
                 " years old and has a GPA of " + gpa);
     }
